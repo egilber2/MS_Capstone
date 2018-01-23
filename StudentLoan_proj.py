4fd17bd11b101c6dd686394f9a854f7d02a7b4f3
Eric Gilbert <eric.gilbert@comcast.net>
2018-01-23 06:19:46 -0500

    Update workflow

--- StudentLoan_proj.py
+++ StudentLoan_proj.py
@@ -32,78 +32,71 @@ from xgboost import XGBRegressor as xgb
 from sklearn.externals import joblib
 
 #%% Import data
-df=pd.read_csv('train_values.csv',
+df_train=pd.read_csv('train_values.csv',
                index_col='row_id')
 
-df1=pd.read_csv('train_labels.csv',
+df_label=pd.read_csv('train_labels.csv',
                 index_col='row_id')
 
-dft=pd.read_csv('test_values.csv',
+df_test=pd.read_csv('test_values.csv',
                 index_col='row_id')
 
 #%% check train data
-df.info()
-df.head()
+df_train.info()
+df_train.head()
 
 #%% check train labels
-df1.info()
-df1.head()
+df_label.info()
+df_label.head()
 
 #%%plot histogram of repayment_rate
 #histogram of 'label'#
 sns.color_palette(palette='deep')
 sns.set_context("notebook")
 sns.set_style("ticks")
-sns.distplot(df1.repayment_rate, bins=20)
+sns.distplot(df_label.repayment_rate, bins=20)
 plt.title('Distribution of Repayment Rate');
 
-
+#%%
 #==============================================================================
 # Process Data
 #==============================================================================
+train=df_train.isnull().sum()  #try label for legend?
+test=df_test.isnull().sum()
+plt.hist(train, bins=20, label='Train data')
+plt.hist(test, bins=20, label='Test data', alpha=0.7)
+plt.title('Counts of Missing Frequency')
+plt.xlabel('Counts of Missing')
+plt.ylabel('Frequency')
+plt.legend(loc='upper right')
+plt.show;
 
+#%% remove features with >20% of data missing.
+df_train.dropna(axis=1, thresh=6970, inplace=True)
 
-#%% #Combine the training and testing data for preprocessing 
-df_comb=pd.concat([df, dft],
-                  axis='rows')
+#%% 
+df_train.info()
 
-#%% view info of combined data set
-df_comb.info()
+#%%  
+###############
+# Categorical
+###############
 
-#%%  plot of missing counts in combined data set
-df_comb.isnull().sum().plot(kind='hist', bins=20);
+#%% 
+df_traincat=df_train.select_dtypes(['object']).apply(pd.Series.astype, dtype='category')
 
-#%% remove features with >10% missing
-df_comb_NAfilter = df_comb.dropna(axis=1,
-                                  thresh=12100)
-
-#%% info
-df_comb_NAfilter.info()
+#%% #display object features to be converted to category
+df_traincat.info()
+df_traincat.head()
 
 #%%
+#confirm that objects been converted to category and get NA count
+df_traincat.isnull().sum()
 
-###################
-# Categorical 
-###################
+#%%
+cat_cols=list(df_traincat)
 
-
-# create data frame containing only object class
-df_obj=df_comb_NAfilter.select_dtypes(['object'])
-
-#%%display object features to be converted to category
-df_obj.describe()
-df_obj.head()
-
-#%% convert object features to categorical
-df_cat=df_obj.apply(pd.Series.astype,
-                    dtype='category')
-
-df_cat.info()
-
-#%%check missin value count 
-df_cat.isnull().sum()
-
-#%%list of categorical features to be removed
+#%%#list of categorical features to be removed
 drop_cols=['report_year',
            'school__online_only',
            'school__religious_affiliation', 
@@ -111,17 +104,17 @@ drop_cols=['report_year',
            'school__main_campus',
            'school__degrees_awarded_predominant']
 
-#%%drop select categorical columns
-df_cat.drop(drop_cols,
-            axis=1,
-            inplace=True)
+#%% #drop select categorical columns
+df_traincat.drop(drop_cols, 
+                 axis=1,
+                 inplace=True)
 
+#%%# fill missing value in region_id with mode
+df_traincat['school__region_id'].fillna(df_traincat['school__region_id'].mode()[0], inplace=True)
 
-#%%fill missing value in region_id with mode
-df_cat['school__region_id'].fillna(df_cat['school__region_id'].mode()[0], inplace=True)
-
-#%% generate dummy variables for remaining categorical features
-df_catDum=pd.get_dummies(df_cat, 
+#%%
+#generate dummy variables for remaining categorical features
+df_traincat=pd.get_dummies(df_traincat, 
                          columns=['school__degrees_awarded_highest', 
                                   'school__institutional_characteristics_level', 
                                   'school__ownership',
@@ -132,9 +125,22 @@ df_catDum=pd.get_dummies(df_cat,
                                           'region'], 
                                   drop_first=True)
 
-#%% info and head of categorical df
-df_catDum.info()
-df_catDum.head()
+#%%
+# info and head of categorical df
+df_traincat.info()
+df_traincat.head()
+
+#%%
+df_traincat.isnull().sum().sum()
+
+#%% 
+df_train.drop(cat_cols, axis=1, inplace=True)
+df_train.info()
+
+#%% 
+df_trainTsf=pd.concat([df_train, df_traincat], axis='columns')
+df_trainTsf.to_csv('df_trainTsf.csv')
+df_trainTsf.info()
 
 #%% 
 
@@ -143,110 +149,138 @@ df_catDum.head()
 ###############
 
 
-#missing values in numeric
-df_num=df_comb_NAfilter.select_dtypes(['float', 'int64'])
-
-#%%Plot missing values in numeric df
-df_num.isnull().sum().plot(kind='hist')
-
-#%%impute missing numeric values with mode 
-
-fill_NaN=Imputer(strategy='most_frequent', axis=0)
-imputed_num=pd.DataFrame(fill_NaN.fit_transform(df_num))
-imputed_num.columns=df_num.columns
-imputed_num.index=df_num.index
+# based on redundance of features as seen in feature importances- first pass
+drop_cols=['student__demographics_median_family_income']
 
 #%%
-imputed_num.isnull().sum().sum()
-imputed_num.info()
+df_trainTsf.drop(drop_cols, axis='columns', inplace=True)
+
+#%%
+X=df_trainTsf.copy()
+# X=df_trainTsf[X_trainRFE.columns]  after feature selection with RFE
+y=df_label.copy()
+
+#%%
+X_train, X_test, y_train, y_test = train_test_split(X,
+                                                    y,
+                                                    test_size=0.2,
+                                                    random_state=7811)
         
-#%%
-imputed_num.select_dtypes(['float64']).skew().hist(bins=30);
+#%%Plot missing values in numeric df
+train=X_train.isnull().sum() #try label for legend?
+test=X_test.isnull().sum()
+plt.hist(train, bins=20, label='X_train')
+plt.hist(test, bins=20, label='X_test', alpha=0.7)
+plt.title('Counts of Missing Values for X_Train and X_test')
+plt.xlabel('Counts of Missing')
+plt.ylabel('Frequency')
+plt.legend(loc='upper right')
+plt.show();
+
+#%%# instantiate imputer for missing values
+imputer=Imputer(strategy='most_frequent', axis=0).fit(X_train)
 
 #%%
-df_trainTsf=pd.concat([df_catDum, imputed_num], axis='columns')
+X_train_imp=imputer.transform(X_train)
 
 #%%
+# turn matrix into df with column names
+index=X_train.index
 
+X_train_df=pd.DataFrame(X_train_imp, columns = X_train.columns, index=index)
+X_train_df.info() 
+X_train_df.head()
+
+#%% 
+X_test_imp=imputer.transform(X_test)
+
+#%%
+index=X_test.index
+X_test_df=pd.DataFrame(X_test_imp, columns=X_test.columns, index=index)
+
+#%%
+X_test_df.head()
+
+#%%
 # =============================================================================
 # Feature Selection
 # =============================================================================
 
-#####################
-# Variance Threshold
-#####################
-
-#remove features with <5% variance
-vt=VarianceThreshold(threshold=0.05)
-vt.fit(imputed_num)
-
-#%%select out chosen features based on variance threshold
-df_numImpvar=imputed_num.iloc[:,vt.get_support()]
-df_numImpvar.info()
-
-#%% combine transformed categorical and numeric dataframes
-df_combTsf=pd.concat([df_catDum, df_numImpvar], axis='columns')
-df_combTsf.info()  #removes 185 features
-
-#%%split back into train and test dataframes
-df_trainTsf=df_combTsf[:8705]
-df_testTsf=df_combTsf[8705:]
-
-#%%inspect transformed train set
-df_trainTsf.info()
-df_trainTsf.head()
-
-#%%inspect transformed test set
-df_testTsf.info()
-df_testTsf.head()
-
-#%% append train values to train set
-trainTsf=pd.concat([df_trainTsf, df1], axis='columns')
-
-y=trainTsf['repayment_rate'].copy()
-X=trainTsf.drop('repayment_rate', axis='columns')
-
-
-#%%
-
-#########
-# RFE
-#########
-
+##-RFE-##
+#%% 
+# Gradient Boosting Regressor for RFE
 estimator = GradientBoostingRegressor(random_state=7811)
-selector = RFECV(estimator,step=1, scoring='neg_mean_squared_error', cv=5, n_jobs=-1)
-selector=selector.fit(X, y)
+
+selector = RFECV(estimator,step=1,
+                 scoring='neg_mean_squared_error',
+                 cv=5,
+                 n_jobs=-1)
+
+selector=selector.fit(X_train_df, y_train.values.ravel())
 print("Optimal number of features : %d" % selector.n_features_)
-#Optimal number of features : 93- fillna 'most_frequent'
+#Optimal number of features : 78- fillna 'most_frequent'
+
 
 #%%
-trainRFE_gboost=X.iloc[:,selector.get_support()]
+joblib.dump(selector, 'rfe_selector.pkl')
 
 #%%
-trainRFE_gboost.to_csv('trainRFE_gboost.csv')
+# Plot number of features VS. cross-validation scores
+plt.figure()
+plt.xlabel("Number of features selected")
+plt.ylabel("Cross validation score (nb of correct classifications)")
+plt.plot(range(1, len(selector.grid_scores_) + 1), selector.grid_scores_)
+plt.show()
 
 #%%
-X_train, X_test, y_train, y_test = train_test_split(trainRFE_gboost,
-                                                    y, 
-                                                    test_size=0.3, 
-                                                    random_state=7811)
+RFEselector=joblib.load('rfe_selector.pkl')
 
 #%%
-estimator.fit(X_train, y_train)
-
-#%%
-y_pred = estimator.predict(X_test)
+y_pred=RFEselector.predict(X_test_df)
 
 mse_scores= mean_squared_error(y_test, y_pred)
 rmse_scores=np.sqrt(mse_scores)
 print(rmse_scores.mean())
 
 #%%
-importances = pd.DataFrame({'feature':X_train.columns,'importance':np.round(estimator.feature_importances_,3)})
+# df_trainRFE=df_trainTsf.iloc[:,selector.get_support()]
+X_trainRFE=X_train_df.iloc[:,selector.get_support()]
+
+X_testRFE=X_test_df.iloc[:,selector.get_support()]
+
+#%%
+X_trainRFE.head()
+y_train.head()
+
+#%%
+X_trainRFE.to_csv('X_trainRFE.csv', index_label='row_id')
+X_testRFE.to_csv('X_testRFE.csv', index_label='row_id')
+
+#%%
+X_trainRFE=pd.read_csv('X_trainRFE.csv', index_col='row_id')
+X_testRFE=pd.read_csv('X_testRFE.csv', index_col='row_id')
+
+#%%
+X_trainRFE.head()
+X_testRFE.head()
+
+#%%
+estimator.fit(X_trainRFE, y_train.values.ravel())
+
+#%%
+y_pred = estimator.predict(X_testRFE)
+
+#%%
+importances = pd.DataFrame({'feature':X_trainRFE.columns,'importance':np.round(estimator.feature_importances_,3)})
 
 importances = importances.sort_values('importance',ascending=False).set_index('feature')
 importances.to_csv('importantRFE_gboost')
 
+#%%
+#Have a look at top 10 features ranked by importance
+importances = importances.sort_values('importance',ascending=False)
+
+importances.head(n=10)
 #%%
 
 #==============================================================================
